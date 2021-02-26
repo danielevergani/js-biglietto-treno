83c305730ccce646238bd15e027b7054ffdff16b
danielevergani <dvergani94@gmail.com>
2021-02-26 15:56:40 +0100

    terminato programma
    ora modifiche + esperimenti

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // calcolo del prezzo del biglietto
 
 // 1. inserimento dati base, km, età, inizializzazione tariffakm
+alert("benvenuto nel nostro calcolatore di prezzo")
 
 var km = parseInt(prompt("quanti chilometri percorrerai?"));
 
@@ -8,7 +9,11 @@ if ( isNaN(km) ){   //validazione dato
   km = parseInt(prompt("devi scrivere un numero"))
 }
 
-console.log(km);
+if ( km > 16723 ){
+  alert("sa vero che le linee attive in Italia hanno una lunghezza massima di 16723 vero? non le sembra eccessivo? ")
+}
+
+document.getElementById("lunghezza_tratta").innerHTML = km;
 
 var eta = parseInt(prompt("quanti anni hai?"));
 
@@ -24,7 +29,7 @@ var tariffaKm = 0.21;
 
 var prezzoStandard = km * tariffaKm;
 
-console.log(prezzoStandard);
+document.getElementById("tariffa_base").innerHTML = prezzoStandard;
 
 // 3. calcolo prezzo scontato
 
@@ -33,10 +38,16 @@ var convSconto = "";
 if (eta < 18){
   convSconto = prezzoStandard * 0.2
   prezzoScontato = prezzoStandard - convSconto
+  // document.getElementById("importo_sconto").innerHTML = convSconto.toFixed(2);
+  // document.getElementById("prezzo_scontato").innerHTML = prezzoScontato.toFixed(2);
+  document.writeln('lei ha diritto a uno sconto di ' + convSconto.toFixed(2) + '&euro; quindi il suo totale è di ' + prezzoScontato.toFixed(2) + '&euro;');
 }
 else if(eta >= 65){
   convSconto = prezzoStandard * 0.4
   prezzoScontato = prezzoStandard - convSconto
+  // document.getElementById("importo_sconto").innerHTML = convSconto.toFixed(2);
+  // document.getElementById("prezzo_scontato").innerHTML = prezzoScontato.toFixed(2);
+  document.writeln('lei ha diritto a uno sconto di ' + convSconto.toFixed(2) + '&euro; quindi il suo totale è di ' + prezzoScontato.toFixed(2) + '&euro;');
 }
 else{
   prezzoScontato = prezzoStandard
